2b113e4a05abf1e217f3d9f56d3dcb9646d3f412
furbSpace <zacharyfurbush@gmail.com>
2025-11-18 23:19:55 -0500

    khonsu has risen... again

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Shu
-Shu is an atmostphere simulation module. 
+# Khonsu
+Khonsu is a physics simulation program written in Python 3.14. 
--- khonsu.py
+++ khonsu.py
@@ -1,10 +1,6 @@
-print("☁️  Loading Shu!")
-
 import os
 import sys
 import time 
-
-print("☁️  Shu Loaded!")
 
 if __name__ == "__main__":
     # Clearing terminal for clarity
@@ -13,25 +9,28 @@ if __name__ == "__main__":
     elif sys.platform == 'win32':
         os.system('cls')
     else:
-        print('⚠️  Unable to clear terminal!')
+        print('⚠️ Unable to clear terminal!')
 
-    print("☁️  Running Shu!")
+    # Announce application start
+    print("🌙 Running Khonsu!")
 
     # Initialize variables
     start_time = time.time()
     elapsed_time = 0
-    print("\n> 🕑  Start (UNIX): {:.0f}".format(start_time)) 
+    print("\n🕑 Start (UNIX): {:.0f}".format(start_time)) 
     max_time = 5
     time_step = 1/10 
 
     # Main loop
     while elapsed_time < max_time:
+
+
         time.sleep(time_step)
         current_time = time.time()
         elapsed_time = current_time - start_time
-        print("\r> ⏳  Elapsed time: {:.1f}s".format(elapsed_time), end='')
+        print("\r⏳ Elapsed time: {:.1f}s".format(elapsed_time), end='')
 
     # Exiting
-    print("\n> 🕓  Finish (UNIX): {:.0f}".format(current_time))
-    print("\n☁️  Exiting Shu!")
+    print("\n🕓 Finish (UNIX): {:.0f}".format(current_time))
+    print("\n🌙 Exiting Khonsu!")
     sys.exit()
